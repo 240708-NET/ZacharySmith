6398e5788461fd3104e041f5c69024176841e209
ZSmith898 <zachary.smith898@gmail.com>
2024-07-10 16:41:45 -0500

    Project 0 Frame Created

---
+++ Project0/Program.cs
@@ -0,0 +1,39 @@
+﻿// The Menu
+//
+
+
+//User will select a Appetizer
+//User will select a Dinner
+//User will select a Desert
+//User will recieve a Bill
+
+
+
+//Classes
+public class Food
+{
+
+}
+public class Appetizer:Food
+{
+
+}
+public class Dinner:Food
+{
+
+}
+public class Dessert:Food
+{
+
+}
+
+//Main
+class Prject0 { 
+  
+    // Main Method 
+    static public void Main(String[] args) 
+    { 
+  
+        
+    } 
+} 
